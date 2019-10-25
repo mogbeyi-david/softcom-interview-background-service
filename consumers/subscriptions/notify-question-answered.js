801117ab54ccd437bcc9433b41154ee2dcc0d935
MOGBEYI David <mogbeyidavid@gmail.com>
2019-10-25 10:37:50 +0100

    pulled in dependencies

--- consumers/subscriptions/notify-question-answered.js
+++ consumers/subscriptions/notify-question-answered.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const connectionString = require("../../config/rabbitmq/connection");
 const open = require('amqplib').connect(connectionString);
 const queue = process.env.NOTIFY_SUBSCRIBERS_TO_QUESTION_QUEUE;
+const SubscriptionRepository = require("../../repositories/SubscriptionRepository");
 
 console.log(`Waiting for data in`, queue);
 
@@ -9,9 +10,16 @@ open.then(function (conn) {
     return conn.createChannel();
 }).then(function (ch) {
     return ch.assertQueue(queue).then(function (ok) {
-        return ch.consume(queue, function (msg) {
-            if (msg !== null) {
-                console.log(msg.content.toString());
+        return ch.consume(queue, async (message) => {
+            if (message !== null) {
+                try {
+                    const messageObject = JSON.parse(message);
+                    const question = messageObject.question;
+                    const subscribers = await SubscriptionRepository.getAllForQuestion(question);
+                    console.log("Subscribers", subscribers);
+                } catch (e) {
+                    console.log(`An error occurred while consuming ${queue}`, e)
+                }
                 ch.ack(msg);
             }
         });
